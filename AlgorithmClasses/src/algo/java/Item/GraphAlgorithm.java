1d8fb3d8aae84c602b83b4fa4f6d60c6e76c1936
arunprakash1995 <axt161330@gmail.com>
2017-09-18 01:49:38 -0500

    GraphAlgo and Diameter Tree upload

---
+++ AlgorithmClasses/src/algo/java/Item/GraphAlgorithm.java
@@ -0,0 +1,15 @@
+package algo.java.Item;
+
+public class GraphAlgorithm<T> {
+    Graph g;
+    // Algorithm uses a parallel array for storing information about vertices
+    T[] node;
+
+    public GraphAlgorithm(Graph g) {
+	this.g = g;
+    }
+
+    T getVertex(Graph.Vertex u) {
+	return Graph.Vertex.getVertex(node, u);
+    }
+}
